ddbfb9eb9985aa03a6dd067093b860920e513e53
Miguel Juarez <mfjuaarez@gmail.com>
2024-09-03 11:43:38 -0300

    Se agrego la clase menu.cs y se avanzo en program.cs

---
+++ Menu.cs
@@ -0,0 +1,11 @@
+class Menu
+{
+    public static void Dibujar(){
+        Console.WriteLine(@"_______
+            (1) Dar de alta un Pedido.
+            (2) Asignar Pedido a cadete.
+            (3) Cambiar estado de pedido.
+            (4) Reasignar pedido
+            (0) salir");
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,20 @@
 ﻿
+Console.Clear();
+Console.WriteLine("Cargando Datos...");
+Thread.Sleep(1000);
+Console.Clear();
 
-Console.WriteLine(@"->            BIENVENIDO            <-");
+var listadoCadetes = Datos.GetCadetes();
+var Cadeteria = Datos.GetCadeteria();
+if (listadoCadetes == null || Cadeteria == null)
+{
+    Console.WriteLine("No es posible iniciar la aplicacion.");
+}else
+{
+    Console.WriteLine(@"->            BIENVENIDO            <-");
+    Menu.Dibujar();
+}
+
 
 
 
